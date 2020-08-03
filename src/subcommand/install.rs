7e59d944d100e7ecbf5ba3a36a9339639e388f8c
Adam <adam.lj.haglund@gmail.com>
2020-08-03 18:01:54 +0200

    add extraction for unix

--- src/subcommand/install.rs
+++ src/subcommand/install.rs
@@ -2,13 +2,17 @@ use std::{
     borrow::Borrow,
     fs::{create_dir_all, File},
     io,
-    io::Cursor,
+    io::{Cursor, Error},
     path::PathBuf,
 };
 
 use clap::ArgMatches;
+#[cfg(unix)]
+use flate2::read::GzDecoder;
 use reqwest::blocking::Response;
 use semver::VersionReq;
+#[cfg(unix)]
+use tar::{Archive, Unpacked};
 #[cfg(target_os = "windows")]
 use zip::ZipArchive;
 
@@ -37,7 +41,7 @@ impl Install {
             let new_path: PathBuf = if let Some(index) = file_path.find('\\') {
                 let mut path = CONFIG.dir().clone();
                 path.push(version_str.clone());
-                path.push(file_path[index + 1..].to_string());
+                path.push(file_path[index + 1..].to_owned());
 
                 path
             } else {
@@ -58,6 +62,67 @@ impl Install {
                 io::copy(&mut item, &mut file)
                     .expect(format!("Couldn't write to {:?}", new_path).borrow());
             }
+        }
+
+        Result::Ok(())
+    }
+
+    #[cfg(unix)]
+    fn extract_archive(bytes: Response, version: &OnlineNodeVersion) -> Result<(), String> {
+        let version_str = version.version().to_string();
+        let base_path = CONFIG.dir();
+
+        let reader = Cursor::new(bytes.bytes().unwrap());
+        let tar = GzDecoder::new(reader);
+        let mut archive = Archive::new(tar);
+
+        let mut version_dir_path = base_path.clone();
+        version_dir_path.push(version_str.to_owned());
+        create_dir_all(version_dir_path.to_owned()).expect("fuck");
+
+        let result = archive
+            .entries()
+            .map_err(|err| err.to_string())?
+            .filter_map(|e| e.ok())
+            .map(|mut entry| -> Result<Unpacked, Error> {
+                let file_path = entry.path()?.to_owned();
+                let file_path = file_path.to_str().unwrap();
+
+                let new_path: PathBuf = if let Some(index) = file_path.find('/') {
+                    let mut path = base_path.clone();
+                    path.push(version_str.clone());
+                    path.push(file_path[index + 1..].to_owned());
+
+                    path
+                } else {
+                    // This happens if it's the root index, the base folder
+                    let mut path = base_path.clone();
+                    path.push(version_str.clone());
+
+                    path
+                };
+
+                entry.set_preserve_permissions(false);
+                entry.unpack(&new_path)
+            });
+
+        let errors: Vec<Error> = result
+            .into_iter()
+            .filter(|result| result.is_err())
+            .map(|result| result.unwrap_err())
+            .collect();
+
+        if !errors.is_empty() {
+            println!(
+                "Failed to extract all files:\n{:?}",
+                errors
+                    .into_iter()
+                    .map(|err| err.to_string())
+                    .collect::<Vec<String>>()
+                    .join("/n")
+            );
+
+            remove_dir_all(version_dir_path).expect("Couldn't clean up version.");
         }
 
         Result::Ok(())
