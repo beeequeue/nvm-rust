c1b16ceae7dac70d0035245f912655fa92a18167
Adam <adam.lj.haglund@gmail.com>
2020-08-20 16:55:31 +0200

    wip add switch integration test

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,14 +1,64 @@
-use std::path::PathBuf;
+use std::{
+    env::set_var,
+    fs::{canonicalize, copy, create_dir_all, read_dir, remove_file},
+    path::PathBuf,
+};
 
-pub fn setup_version(version_stub: &str) {
-    let path: PathBuf = ["test-data", "versions", version_stub].iter().collect();
+pub const INTEGRATION_DIR: &str = "./integration";
 
-    if !path.exists() {
+pub fn integration_dir() -> PathBuf {
+    PathBuf::from(INTEGRATION_DIR)
+}
+
+pub fn required_files<'a>() -> [&'a str; 4] {
+    ["node", "node.cmd", "npm", "npm.cmd"]
+}
+
+pub fn setup_integration_test() -> Result<(), anyhow::Error> {
+    set_var("NVM_DIR", INTEGRATION_DIR);
+
+    let path = integration_dir();
+    for entry in read_dir(path.to_owned())? {
+        let name = entry?.file_name();
+        remove_file(path.join(name))?
+    }
+
+    Result::Ok(())
+}
+
+pub fn install_mock_version(version_str: &str) -> Result<(), anyhow::Error> {
+    let test_data_dir = PathBuf::from("test-data")
+        .join("versions")
+        .join(version_str);
+    let test_data_dir = canonicalize(test_data_dir).expect("Could not resolve stub version path");
+
+    if !test_data_dir.exists() {
         panic!(
             "Tried to set up mock version {} which doesn't exist.",
-            version_stub
+            version_str
         );
     }
 
-    println!("{:#?}", path);
+    let to_dir = PathBuf::from(INTEGRATION_DIR).join(version_str);
+    create_dir_all(to_dir.to_owned())?;
+
+    for entry in read_dir(test_data_dir.to_owned())? {
+        let name = entry?.file_name();
+
+        copy(test_data_dir.join(name.to_owned()), to_dir.join(name))?;
+    }
+
+    Result::Ok(())
+}
+
+pub fn assert_version_installed(version_str: &str) -> Result<(), anyhow::Error> {
+    let path = integration_dir();
+
+    for filename in required_files().iter() {
+        let file_path = path.join(version_str).join(filename);
+        eprintln!("file_path = {:#?}", file_path);
+        assert!(file_path.exists(), "{:#?} was not created", file_path);
+    }
+
+    Result::Ok(())
 }
--- tests/switch_test.rs
+++ tests/switch_test.rs
@@ -1,23 +1,31 @@
-use assert_cmd::Command;
-use serial_test::serial;
-
 mod common;
 
-#[test]
-#[serial]
-fn switches_version() {
-    common::setup_version("12");
+mod switch {
+    use assert_cmd::Command;
+    use serial_test::serial;
 
-    let mut cmd = Command::cargo_bin("nvm-rust").unwrap();
+    use crate::{common, common::assert_version_installed};
 
-    let result = cmd
-        .env("NVM_DIR", "C:\\dev\\test")
-        .arg("-V")
-        .arg("use")
-        .arg("12")
-        .assert();
+    #[test]
+    #[serial]
+    fn can_switch_version_with_no_previous_one() -> Result<(), anyhow::Error> {
+        let version_str = "12.18.3";
+        common::setup_integration_test()?;
+        common::install_mock_version(version_str)?;
 
-    let output = result.get_output();
+        let mut cmd = Command::cargo_bin("nvm-rust").unwrap();
 
-    println!("{:?}", String::from_utf8(output.stdout.clone()).unwrap());
+        let result = cmd.arg("use").arg("12").assert();
+
+        let output = result.get_output();
+
+        println!(
+            "{}",
+            String::from_utf8(output.stdout.clone()).unwrap().trim()
+        );
+
+        assert_version_installed(version_str)?;
+
+        Result::Ok(())
+    }
 }
