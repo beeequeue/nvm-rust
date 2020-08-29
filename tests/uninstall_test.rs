6fb701cb01dfd91f9525f504f031ef251b8274a8
Adam <adam.lj.haglund@gmail.com>
2020-08-29 14:35:46 +0200

    mock multiple versions

--- tests/uninstall_test.rs
+++ tests/uninstall_test.rs
@@ -10,10 +10,14 @@ mod uninstall {
         common::{assert_outputs, assert_version_not_installed},
     };
 
-    fn setup_installed_version(version_str: &str) -> Result<()> {
+    fn setup_versions(versions: Vec<&str>) -> Result<()> {
         common::setup_integration_test()?;
-        common::install_mock_version(version_str)?;
-        common::create_shim(version_str)?;
+
+        versions.to_owned().into_iter().for_each(|version_str| {
+            common::install_mock_version(version_str).expect("Mock version");
+        });
+
+        common::create_shim(versions.get(0).unwrap()).expect("Select mock version");
 
         Result::Ok(())
     }
@@ -22,7 +26,7 @@ mod uninstall {
     #[serial]
     fn can_uninstall_version_matching_range() -> Result<()> {
         let version_str = "12.18.3";
-        setup_installed_version(version_str)?;
+        setup_versions(vec![version_str, "14.5.0"])?;
 
         let mut cmd = Command::cargo_bin("nvm-rust").unwrap();
         let result = cmd.arg("uninstall").arg("12").assert();
@@ -37,10 +41,10 @@ mod uninstall {
     #[serial]
     fn can_uninstall_version_matching_exact_version() -> Result<()> {
         let version_str = "12.18.3";
-        setup_installed_version(version_str)?;
+        setup_versions(vec![version_str, "14.5.0"])?;
 
         let mut cmd = Command::cargo_bin("nvm-rust").unwrap();
-        let result = cmd.arg("uninstall").arg("12.18.3").assert();
+        let result = cmd.arg("uninstall").arg(version_str).assert();
 
         assert_outputs(&result, "Uninstalled 12.18.3!", "")?;
         assert_version_not_installed(version_str)?;
