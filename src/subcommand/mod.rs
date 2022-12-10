47adaab4b8b44a711a5896c8d768f836ce406213
Adam <adam@haglund.dev>
2022-12-10 19:59:03 +0100

    rust fmt --all

--- src/subcommand/mod.rs
+++ src/subcommand/mod.rs
@@ -2,9 +2,9 @@ use anyhow::Result;
 
 use crate::Config;
 
-pub mod list;
-pub mod is_installed;
 pub mod install;
+pub mod is_installed;
+pub mod list;
 pub mod parse_version;
 pub mod switch;
 pub mod uninstall;
